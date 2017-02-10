223c2ccb3a896bd41c28a4118ccd1dac65bd2be3
WinBarries <tbola45@gmail.com>
2017-02-10 13:27:54 -0500

    Update quadraticEquation.cpp
    this c++ code will solve quadratic equation

--- csc/quadraticEquation.cpp
+++ csc/quadraticEquation.cpp
@@ -7,41 +7,69 @@
 ////////////////////////
 
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
-int main() 
+int main()
 {
 	// Declaration
+    float a;
+    float b;
+    float c;
+    float root1;
+    float root2;
+    float isReal;
+
 
 	// Initialization
+    a       = 0.0;
+    b       = 0.0;
+    c       = 0.0;
+    isReal  = 0.0;
+    root1   = 0.0;
+    root2   = 0.0;
 
 	// 1. Ask the user for the coeef of x^2
+    cout << "Please enter the coefficient of x^2: ";
 
 	// 2. Collect the coeef.(i.e a)
+    cin >> a;
 
 	// 3. As the user for the coeef. of x
+	cout << "Please enter the coefficient of x: ";
 
 	// 4. Collect the coeef.(i.e b)
+    cin >> b;
 
 	// 5. Ask the user for the constant
+    cout << "Please enter the constant: ";
 
 	// 6. Collect the constant (i.e c)
+    cin >> c;
 
 	// 7. Calculate (b*b - 4*a*c)
+    isReal = (b*b - 4*a*c);
 
 	// 8. If number was positive
-		
+		if (isReal >= 0)
+        {
+
 		// 8.a. Calculate the first square root:
-			//(x1 = (-b - sqrt(b*b - 4 * a * c)) / (-2 * a))
+			root1 = (-b - sqrt(b*b - 4 * a * c)) / (-2 * a);
 
 		// 8.b. Calculate the second square root:
-			//(x1 = (-b + sqrt(b*b - 4 * a * c)) / (-2 * a))
+            root2 = (-b + sqrt(b*b - 4 * a * c)) / (-2 * a);
 
 		// 8.c. Write down the equation (a x^2 + b x + c)
+            cout << "The solution for equation " << a <<"x^2 + (" << b <<") x + ("<<c<<")"<<endl;
 
 		// 8.d. Display the root calculated
-	
+		cout << "is x1 = "<<root1  << " and x2 = " << root2<<endl;
+        }
+
 	// 9. otherwise
+        else
+
 		// 9.a. Inform the user no real solution
+        cout << "the equation No real solution \n";
 }
